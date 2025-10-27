bd93a5c87cecbfa09e4330be27cbe292803e9a56
Chakhieyw <mongkonwit0075@gmail.com>
2025-10-27 21:26:59 +0700

    ..

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,21 +12,48 @@ import { EditGame } from './page/admin/edit-game/edit-game';
 import { GameLibrary } from './page/user/game-library/game-library';
 import { Component } from '@angular/core';
 import { GameDetailComponent } from './page/user/game-detail/game-detail';
+import { DiscountPage } from './page/admin/discount/discount';
+import { DetailGamePage } from './page/admin/detail-game/detail-game';
+
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
-  { path: 'user/home', component: UserHome , canActivate: [UserGuard] },
-  { path: 'admin/home', component: AdminHome ,canActivate: [AdminGuard] },
-  { path: 'admin/manage-game', component: ManageGame ,canActivate: [AdminGuard] },
-  { path: 'admin/add-game', component: AddGame ,canActivate: [AdminGuard] },
-  { path: 'admin/edit-game/:id', component: EditGame , canActivate: [AdminGuard] },
-  { path: 'user/profile', component: Profile , canActivate: [UserGuard] },
-  { path: 'user/game-library', component: GameLibrary, canActivate: [UserGuard] },
-  { path: 'user/game-detail/:id', component: GameDetailComponent, canActivate: [UserGuard]},
-
-
+  { path: 'user/home', component: UserHome, canActivate: [UserGuard] },
+  { path: 'admin/home', component: AdminHome, canActivate: [AdminGuard] },
+  {
+    path: 'admin/manage-game',
+    component: ManageGame,
+    canActivate: [AdminGuard],
+  },
+  { path: 'admin/add-game', component: AddGame, canActivate: [AdminGuard] },
+  {
+    path: 'admin/edit-game/:id',
+    component: EditGame,
+    canActivate: [AdminGuard],
+  },
+  {
+    path: 'admin/detail-game/:id',
+    component: DetailGamePage,
+    canActivate: [AdminGuard],
+  },
+  {
+    path: 'admin/discount',
+    component: DiscountPage,
+    canActivate: [AdminGuard],
+  },
+  { path: 'user/profile', component: Profile, canActivate: [UserGuard] },
+  {
+    path: 'user/game-library',
+    component: GameLibrary,
+    canActivate: [UserGuard],
+  },
+  {
+    path: 'user/game-detail/:id',
+    component: GameDetailComponent,
+    canActivate: [UserGuard],
+  },
 
   { path: '**', redirectTo: 'login' },
 ];
